12e86e57dd32a1cb6ebd1287aa3f48c8a2ffb57c
Felipe Baisi <felipe.baisi@lexisnexis.com>
2016-10-07 17:00:18 -0300

    Add write txt method

--- app/src/main/java/com/febaisi/activityrecognition/Utils.java
+++ app/src/main/java/com/febaisi/activityrecognition/Utils.java
@@ -1,5 +1,17 @@
 package com.febaisi.activityrecognition;
 
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 /**
  * Created by BaisFe01 on 10/5/2016.
  */
@@ -7,4 +19,23 @@ package com.febaisi.activityrecognition;
 public class Utils {
 
     public static String TAG = "ActivityRecognitionApp";
+
+    public static void writeToSDFile(String message, String fileName){
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File (root.getAbsolutePath() + "/ActivityLogs");
+        dir.mkdirs();
+        File file = new File(dir, fileName + ".txt");
+        try {
+            FileOutputStream f = new FileOutputStream(file, true);
+            PrintWriter pw = new PrintWriter(f);
+            pw.println(message);
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
